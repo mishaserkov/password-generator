ba0b4c309280787502c9da33e40ba646136978ed
Misha Serkov <m.serkov@gmail.com>
2024-12-07 12:41:28 +0100

    feat: implement password settings in html and css

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOne = document.querySelector("#password-one");
-let passwordTwo = document.querySelector("#password-two");
+let passwordLengthRange = document.querySelector("#password-length-range");
+let passwordLengthValue = document.querySelector("#password-length-value");
 
-function generatePassword() {
-    let generatedPasswordOne = "";
-    let generatedPasswordTwo = "";
+passwordLengthRange.addEventListener("input", () => {
+    passwordLengthValue.value = passwordLengthRange.value;
+});
 
-    for(i = 0; i < 15; i++) {
-        generatedPasswordOne += characters[Math.floor(Math.random() * characters.length)]
-        generatedPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-    }
+
+// let passwordOne = document.querySelector("#password-one");
+// let passwordTwo = document.querySelector("#password-two");
+
+// function generatePassword() {
+//     let generatedPasswordOne = "";
+//     let generatedPasswordTwo = "";
+
+//     for(i = 0; i < 15; i++) {
+//         generatedPasswordOne += characters[Math.floor(Math.random() * characters.length)]
+//         generatedPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
+//     }
     
-    passwordOne.textContent = generatedPasswordOne;
-    passwordTwo.textContent = generatedPasswordTwo;
-}
+//     passwordOne.textContent = generatedPasswordOne;
+//     passwordTwo.textContent = generatedPasswordTwo;
+// }
 
 /*
 Stretch goals:
@@ -23,3 +31,7 @@ Stretch goals:
 - Add "copy-on-click"
 - Toggle "symbols" and "numbers on/off"
 */
+
+
+
+
